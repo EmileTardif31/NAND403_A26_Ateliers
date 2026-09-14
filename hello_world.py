5a084227537fbfaca63b4cdc1bf2fcc19e4218ac
EmileTardif31 <emiletardif31@gmail.com>
2026-09-14 18:38:28 -0400

    ajout du fichier test du cours 2

---
+++ hello_world.py
@@ -0,0 +1,27 @@
+# print("Hello World !")
+
+# v = 1
+
+# def main():
+#     print(v)
+
+# main()
+
+
+import sys
+
+this_is_a_variable = 1 # what de teacher want
+# aussi ca doit etre ca pour le nom des fonctions
+
+thisIsAVariable = 1 # not ok apparentli
+
+
+print("allo")
+
+def my_good_function(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+
+
+my_good_function("Will", 1.2, True)
